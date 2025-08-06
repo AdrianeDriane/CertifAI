134d6fe630b801a1b0e0409a9df28c68a85ecdc8
AdrianeDriane <dilaoaparre@gmail.com>
2025-08-06 21:11:59 +0800

    feat: get documents backend

--- server/src/routes/documentRoutes.ts
+++ server/src/routes/documentRoutes.ts
@@ -1,9 +1,28 @@
-import express from 'express';
-import { createNewDocument } from '../controllers/documentControllers';
-import { authenticate } from '../middlewares/authMiddleware';
+import express, { Request, Response } from "express";
+import { createNewDocument } from "../controllers/documentControllers";
+import { authenticate } from "../middlewares/authMiddleware";
+import Document from "../models/Document";
 
 const router = express.Router();
 
-router.post('/', authenticate, createNewDocument);
+router.post("/", authenticate, createNewDocument);
+router.get("/", authenticate, async (req: Request, res: Response) => {
+  try {
+    const user = req.user as { _id: String };
+
+    if (!user) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    const documents = await Document.find({ createdBy: user._id });
+
+    res.status(200).json({ documents });
+    return;
+  } catch (error) {
+    console.error("Error fetching documents: ", error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
 
 export default router;
