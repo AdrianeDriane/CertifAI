b179dc0f36a9337bcd6e8ad008c78e030e637cc7
AdrianeDriane <dilaoaparre@gmail.com>
2025-08-06 21:23:46 +0800

    feat: get documents controller and fixed improper id call

--- server/src/routes/documentRoutes.ts
+++ server/src/routes/documentRoutes.ts
@@ -1,28 +1,13 @@
-import express, { Request, Response } from "express";
-import { createNewDocument } from "../controllers/documentControllers";
+import express from "express";
+import {
+  createNewDocument,
+  getDocuments,
+} from "../controllers/documentControllers";
 import { authenticate } from "../middlewares/authMiddleware";
-import Document from "../models/Document";
 
 const router = express.Router();
 
 router.post("/", authenticate, createNewDocument);
-router.get("/", authenticate, async (req: Request, res: Response) => {
-  try {
-    const user = req.user as { _id: String };
-
-    if (!user) {
-      res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
-
-    const documents = await Document.find({ createdBy: user._id });
-
-    res.status(200).json({ documents });
-    return;
-  } catch (error) {
-    console.error("Error fetching documents: ", error);
-    res.status(500).json({ message: "Internal server error", error });
-  }
-});
+router.get("/", authenticate, getDocuments);
 
 export default router;
