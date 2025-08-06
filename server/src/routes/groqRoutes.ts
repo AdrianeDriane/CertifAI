7d2379d2abff23b8458d26c2c7719ee631f671f2
AdrianeDriane <dilaoaparre@gmail.com>
2025-08-06 19:35:37 +0800

    feat: authenticate groq route

--- server/src/routes/groqRoutes.ts
+++ server/src/routes/groqRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { generateDocument } from "../controllers/groqControllers";
+import { authenticate } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.post("/", generateDocument);
+router.post("/", authenticate, generateDocument);
 
 export default router;
